3782f0f5b7a0eacee1c670c61acd9794ac0c0ede
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-25 12:58:48 +0300

    updating the path

--- routers/soldier_router.js
+++ routers/soldier_router.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { soldier_controller } = require('../controllers/soldier_controller');
+const { soldier_controller } = require('./controllers/soldier_controller');
 const soldierRouter = new Router();
 
 router.post('/addSoldier', soldier_controller.addSoldier);
