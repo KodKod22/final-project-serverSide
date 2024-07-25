b2af32f175d065dbc149462034ddc89c36fe627b
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-25 14:26:17 +0300

    changed port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8081;
-const  soldierRouter  = require('./routers/soldier_router');
+const soldierRouter = require('./routers/soldier_router');
+const bodyParser = require('body-parser');
 
 app.use((req, res, next) => {
     res.set({
@@ -15,10 +15,12 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public'));
+
 app.use('/api/soldiers', soldierRouter);
+
 
 app.use((req, res) => {
     console.error('Path not found:', req.path);
