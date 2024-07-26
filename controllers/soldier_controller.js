8b0720ec8c0db45a9d5e117876ffa6e6428b58f3
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-26 11:23:14 +0300

    fixing connection to sql try 1

--- controllers/soldier_controller.js
+++ controllers/soldier_controller.js
@@ -5,13 +5,13 @@ exports.soldier_controller = {
         const { body } = req;
         try {
             const result = await connection.execute(
-                `INSERT INTO tbl_111_soldiers(s_id, soldier_name, s_role, s_rank, years_in_unit, riflery, date_of_birth, s_img) 
+                `INSERT INTO tbl_111_soldiers(soldierID, name, role, rank, yearsInTheUnit, riflery, dateOfBirth, s_img) 
                  VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
                 [body.s_id, body.soldier_name, body.s_role, body.s_rank, body.years_in_unit, body.riflery, body.date_of_birth, body.s_img]
             );
     
             connection.end();
-            res.status(201).json({ success: true, accessCode });
+            res.status(201).json({ success: true });
         } catch (error) {
             console.error('Error inserting user:', error);
             res.status(500).send(false);
