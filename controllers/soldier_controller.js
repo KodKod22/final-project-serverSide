763f53f10e092edf7874584cbbc2177169420ec2
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-26 12:36:04 +0300

    .

--- controllers/soldier_controller.js
+++ controllers/soldier_controller.js
@@ -5,7 +5,7 @@ exports.soldier_controller = {
         const { body } = req;
         try {
             const result = await db.execute(
-                `INSERT INTO tbl_111_soldiers(soldierID, name, role, rank, yearsInTheUnit, riflery, dateOfBirth, s_img) 
+                `INSERT INTO tbl_111_soldiers(soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img) 
                  VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
                 [body.s_id, body.soldier_name, body.s_role, body.s_rank, body.years_in_unit, body.riflery, body.date_of_birth, body.s_img]
             );
