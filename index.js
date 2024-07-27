5329fcf8c304871db707c9dfc71070c64e5f126c
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-27 13:20:55 +0300

    trying to get app

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const db = require('./db_connection');
 db.dbConnection.createConnection();
 
 const soldierRouter = require('./routers/soldier_router');
+const app = require('./server/main').getApp();
--- routers/soldier_router.js
+++ routers/soldier_router.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const ms = require('../server/main');
+const { upload } = require('../server/main');
 const { addSoldier } = require('../controllers/soldier_controller');
 
 const router = express.Router();
 
-router.post('/api/soldiers/addSoldier', ms.upload.single('s_img'), addSoldier);
+router.post('addSoldier', upload.single('s_img'), addSoldier);
 
 module.exports = router;
