5ad17a94cc24ee6a45ed4d2c64c24bedd171a91e
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-27 12:11:10 +0300

    removing one of the exports

--- controllers/soldier_controller.js
+++ controllers/soldier_controller.js
@@ -1,27 +1,26 @@
-exports.soldier_controller = {
-    async addSoldier(req, res) {
-        const { dbConnection } = require('../db_connection');
-        let db = dbConnection.getConnection();
-        try {
-            const { soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth } = req.body;
-            const s_img = req.file;
-    
-            console.log('Received data:', { soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img });
-    
-            if (!soldierID || !name || !role || !rank || !yearsInTheUnits || !riflery || !dateOfBirth || !s_img) {
-                throw new Error('Missing required fields');
-            }
+const { dbConnection } = require('../db_connection');
 
-            const [result] = await db.execute(
-                'INSERT INTO soldiers (soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                [soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img.buffer]
-            );
+async function addSoldier(req, res) {
+    let db = dbConnection.getConnection();
+    try {
+        const { soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth } = req.body;
+        const s_img = req.file;
     
-            res.status(200).json({ message: 'Soldier added successfully' });
-        } catch (error) {
-            console.error('Error inserting user:', error);
-            res.status(500).send(false);
+        console.log('Received data:', { soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img });
+    
+        if (!soldierID || !name || !role || !rank || !yearsInTheUnits || !riflery || !dateOfBirth || !s_img) {
+            throw new Error('Missing required fields');
         }
+
+        const [result] = await db.execute(
+            'INSERT INTO soldiers (soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img.buffer]
+        );
+    
+        res.status(200).json({ message: 'Soldier added successfully' });
+    } catch (error) {
+        console.error('Error inserting user:', error);
+        res.status(500).send(false);
     }
 }
 
