195ccbc2d89e2e413c81759dc4427e3deb65e151
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-07-29 14:06:44 +0300

    .

--- controllers/soldier_controller.js
+++ controllers/soldier_controller.js
@@ -3,6 +3,7 @@ const db = require('../db_connection');
 exports.soldier_controller = {
     async addSoldier(req, res) {
         const { body } = req;
+        console.log(body)
         try {
             const result = await db.pQuery(
                 `INSERT INTO tbl_111_soldiers(soldierID, name, role, rank, yearsInTheUnits, riflery, dateOfBirth, s_img) 
