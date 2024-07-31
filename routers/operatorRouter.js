a7f7467495f08e6c5532c209306332e675f47389
kodyAgin <kodyagin@gmail.com>
2024-07-31 22:04:00 +0300

    changing post user to getuser

--- routers/operatorRouter.js
+++ routers/operatorRouter.js
@@ -6,7 +6,7 @@ const { authMiddleware} = require('../middleware/auth.js');
 operatorRouter.post('/addMission',authMiddleware.getSoldierID,operatorController.insertSoldierMission);
 operatorRouter.get('/SimulationsRecords',operatorController.getSimulationsRecords);
 operatorRouter.get('/Simulations',operatorController.getSimulations);
-operatorRouter.post('/user',operatorController.getUser);
+operatorRouter.get('/user',operatorController.getUser);
 operatorRouter.delete('/deleteSimulations',operatorController.deleteSimulations);
 operatorRouter.put('/updateSimulation', operatorController.updateSimulations);
 
