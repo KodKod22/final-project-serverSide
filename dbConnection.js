8478f95e88398773f76783dfd3f93ad078d06788
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-08-01 08:19:20 +0300

    adding debug to the connection

--- dbConnection.js
+++ dbConnection.js
@@ -1,6 +1,11 @@
 exports.dbConnection = {
     async createConnection() {
         const mysql = require('mysql2/promise');
+
+        console.log(process.env.DB_HOST);
+        console.log( process.env.DB_USERNAME);
+        console.log(process.env.DB_PASSWORD);
+        console.log(process.env.DB_NAME);
         const connection = await mysql.createConnection({
         host : process.env.DB_HOST,
         user : process.env.DB_USERNAME,
