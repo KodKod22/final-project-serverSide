3d55d5eab280813d1b370d63aa1495d305c15f84
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-08-02 00:16:34 +0300

    updating getSimulationsRecords

--- controllers/soldier_controller.js
+++ controllers/soldier_controller.js
@@ -68,49 +68,54 @@ exports.soldierController = {
     async getSimulationsRecords(req,res){
         const {dbConnection} = require('../dbConnection');
         const connection = await dbConnection.createConnection();
-        const [rows] = await connection.execute(`
-            SELECT 
-                sim.id,
-                sim.simulationID,
-                sim.date,
-                sim.video,
-                sir.simulationName,
-                sir.location,
-                sir.afvToRescue,
-                sir.RescueVehicle,
-                sir.difficulty,
-                s1.name AS CommanderName,
-                s2.name AS DriverName,
-                s3.name AS SafetyOfficerName,
-                s4.name AS TeamMember1Name,
-                s5.name AS TeamMember2Name,
-                s6.name AS TeamMember3Name
-            FROM 
-                tbl_111_simulationRecords sim
-            INNER JOIN 
-                tbl_111_simulations sir ON sir.id = sim.simulationID
-            INNER JOIN 
-                tbl_111_soldiers s1 ON sim.commanderID = s1.soldierID
-            INNER JOIN 
-                tbl_111_soldiers s2 ON sim.driverID = s2.soldierID
-            INNER JOIN 
-                tbl_111_soldiers s3 ON sim.safetyOfficerID = s3.soldierID
-            LEFT JOIN 
-                tbl_111_soldiers s4 ON sim.teamMember1ID = s4.soldierID
-            LEFT JOIN 
-                tbl_111_soldiers s5 ON sim.teamMember2ID = s5.soldierID
-            LEFT JOIN 
-                tbl_111_soldiers s6 ON sim.teamMember3ID = s6.soldierID`);
-        const formattedRows = rows.map(row => {
-            if (row.date && row.date instanceof Date) {
-                row.date = row.date.toISOString().split('T')[0];
-            }
-            return row;
-        });
+        try{
+            const [rows] = await connection.execute(`
+                SELECT 
+                    sim.id,
+                    sim.simulationID,
+                    sim.date,
+                    sim.video,
+                    sir.simulationName,
+                    sir.location,
+                    sir.afvToRescue,
+                    sir.RescueVehicle,
+                    sir.difficulty,
+                    s1.name AS CommanderName,
+                    s2.name AS DriverName,
+                    s3.name AS SafetyOfficerName,
+                    s4.name AS TeamMember1Name,
+                    s5.name AS TeamMember2Name,
+                    s6.name AS TeamMember3Name
+                FROM 
+                    tbl_111_simulationRecords sim
+                INNER JOIN 
+                    tbl_111_simulations sir ON sir.id = sim.simulationID
+                INNER JOIN 
+                    tbl_111_soldiers s1 ON sim.commanderID = s1.soldierID
+                INNER JOIN 
+                    tbl_111_soldiers s2 ON sim.driverID = s2.soldierID
+                INNER JOIN 
+                    tbl_111_soldiers s3 ON sim.safetyOfficerID = s3.soldierID
+                LEFT JOIN 
+                    tbl_111_soldiers s4 ON sim.teamMember1ID = s4.soldierID
+                LEFT JOIN 
+                    tbl_111_soldiers s5 ON sim.teamMember2ID = s5.soldierID
+                LEFT JOIN 
+                    tbl_111_soldiers s6 ON sim.teamMember3ID = s6.soldierID`);
+            const formattedRows = rows.map(row => {
+                if (row.date && row.date instanceof Date) {
+                    row.date = row.date.toISOString().split('T')[0];
+                }
+                return row;
+            });
+    
+            res.status(201).json(rows);
+            connection.end()
+            return formattedRows;   
 
-        res.json(rows);
-        connection.end()
-        return formattedRows;   
+        }catch (error) {
+            res.status(500).json({ message: 'Error fetching records' });
+        }
     }
 }
 
