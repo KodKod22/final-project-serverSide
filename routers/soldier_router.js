ae2f37735e79ce9f9a38605aed8ba2267307c036
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-08-01 17:50:12 +0300

    adding soldier profile

--- routers/soldier_router.js
+++ routers/soldier_router.js
@@ -6,5 +6,7 @@ soldierRouter.post('/addSoldier', soldierController.addSoldier);
 soldierRouter.post('/deleteSoldier', soldierController.deleteSoldier);
 soldierRouter.get('/getRoles', soldierController.getRoles);
 soldierRouter.get('/getSoldiers', soldierController.getSoldiers);
+soldierRouter.post('/getSoldiersProfile', soldierController.getSoldiersProfile);
+
 
 module.exports = {soldierRouter};
