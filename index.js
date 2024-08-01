0b54d05df26f643fd56e2a183a36c8d3267297b2
kodyAgin <kodyagin@gmail.com>
2024-08-01 20:23:29 +0300

    adding get catgorys

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8081;
 const simulationData = require('./data/simulation.json');
-
+const categoryData = require('./data/categories.json');
 const { operatorRouter } = require('./routers/operatorRouter.js');
 const { soldierRouter } = require('./routers/soldier_router.js');
 const { simulationFeedbackRouter } = require('./routers/simulationFeedback_router.js');
 
+app.get("/simulation",(req,res)=>{
+    res.json(simulationData);
+});
+app.get("/categories", (req, res) => { 
+    res.json(categoryData);
+});
 app.use((req, res, next) => {
     res.set({
     'Access-Control-Allow-Origin': '*',
@@ -24,9 +30,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/client/images', express.static(`${__dirname}/images`));
 
-app.get("/simulation",(req,res)=>{
-    res.json(simulationData);
-});
 
 app.use('/api/post',operatorRouter);
 app.use('/api/soldiers',soldierRouter);
@@ -35,9 +38,7 @@ app.use('/api/simulationFeedback',simulationFeedbackRouter);
 app.use((req,res)=>{
     res.status(400).send("Something is broken");
 });
-app.get("/categories", (req, res) => { 
-    res.json(`${__dirname}/data/categories.json`);
-});
+
 
 app.listen(port);
 console.log(`listening on port ${port}`);
