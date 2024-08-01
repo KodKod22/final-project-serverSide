b308bba3875433617a54e5a15c63324200d9eebd
kodyAgin <kodyagin@gmail.com>
2024-08-01 14:59:53 +0300

    changing get users

--- controllers/operatorController.js
+++ controllers/operatorController.js
@@ -16,23 +16,29 @@ exports.operatorController = {
         
         return formattedRows; 
     },
-    async getUser(req,res) {
-        const {dbConnection} = require('../dbConnection');
+    async getUser(req, res) {
+        const { dbConnection } = require('../dbConnection');
         const connection = await dbConnection.createConnection();
         const userName = req.body.name;
         const userPassword = req.body.password;
-        const [rows] = await connection.execute(
-            'SELECT * FROM tbl_111_usersType WHERE user_name = ? AND user_password = ?',
-            [userName, userPassword]
-        );
-
-        if (rows.length === 0) {
-            res.status(401).json({ error: 'Invalid username or password' });
-        } else {
+    
+        try {
+            const [rows] = await connection.execute(
+                'SELECT * FROM tbl_111_usersType WHERE user_name = ? AND user_password = ?',
+                [userName, userPassword]
+            );
+    
             connection.end();
-            res.json(rows[0]);
+    
+            if (rows.length === 0) {
+                return res.status(401).json({ error: 'Invalid username or password' });
+            } else {
+                return res.json(rows[0]);
+            }
+        } catch (error) {
+            console.error('Database error:', error);
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
-        return rows[0];
     },
     async deleteSimulations(req,res) {
         const {dbConnection} = require('../dbConnection');
