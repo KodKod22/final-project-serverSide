f30de06ba13e1ab1807a8d527e3175a82361c9c1
chenweiss7 <117260012+chenweiss7@users.noreply.github.com>
2024-08-01 22:09:33 +0300

    adding app controller and router
    .

--- index.js
+++ index.js
@@ -3,33 +3,39 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8081;
 const simulationData = require('./data/simulation.json');
-const { operatorRouter } = require('./routers/operatorRouter.js');
-const { soldierRouter } = require('./routers/soldier_router.js');
-const { simulationFeedbackRouter } = require('./routers/simulationFeedback_router.js');
+const { operatorRouter } = require('./routers/operatorRouter');
+const { soldierRouter } = require('./routers/soldier_router');
+const { simulationFeedbackRouter } = require('./routers/simulationFeedback_router');
+const userRouter = require('./routers/userRouter');
+const {appRouter}=require('./routers/appRouter');
 
 app.use((req, res, next) => {
     res.set({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
-    'Content-Type': 'application/json'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Content-Type': 'application/json'
     });
     next();
 });
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use("/client/images",express.static(`${__dirname}/images`));
-app.get("/simulation",(req,res)=>{
+app.use(express.urlencoded({ extended: true }));
+app.use('/client/images', express.static(`${__dirname}/images`));
+app.get('/simulation', (req, res) => {
     res.json(simulationData);
 });
 
-app.use('/api/post',operatorRouter);
-app.use('/api/soldiers',soldierRouter);
-app.use('/api/simulationFeedback',simulationFeedbackRouter);
+app.use('/api/post', operatorRouter);
+app.use('/api/soldiers', soldierRouter);
+app.use('/api/simulationFeedback', simulationFeedbackRouter);
+app.use('/api', userRouter);
+app.use('/api/app',appRouter)
 
-app.use((req,res)=>{
-    res.status(400).send("Something is broken");
+app.use((req, res) => {
+    res.status(400).send('Something is broken');
 });
 
-app.listen(port);
-console.log(`listening on port ${port}`);
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
