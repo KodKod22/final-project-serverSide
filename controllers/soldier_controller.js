985e67ef2c10f08b1e85f3e65cd3f2d2fff184a4
ValeriBalan <165626222+ValeriBalan@users.noreply.github.com>
2024-08-04 00:50:14 +0300

    spelling

--- controllers/soldier_controller.js
+++ controllers/soldier_controller.js
@@ -155,8 +155,8 @@ exports.soldierController = {
         const {id} = req.params;
         try{
             const [rows]= await connection.execute(`select * from tbl_111_simulationRecords inner join
-               tbl_111_simulations on tbl_111_simulations.id = tbl_111_simulationRecords.simulationID =
-               where id= ?`,[id]);
+               tbl_111_simulations on tbl_111_simulations.id = tbl_111_simulationRecords.simulationID 
+               where tbl_111_simulationRecords.id= ?`,[id]);
             res.status(201).json(rows);
         }catch(error){
             res.status(500).json({ message: `Error fetching simulation id:${id}`, _id: id });
